6a24574d32669c185a67d4ab79a3c18ef1e953d2
CORP\ploskin.alexander <alexander.ploskin@devexpress.com>
2021-11-02 13:35:00 +0300

    Fix codestyle

--- UndoOperation/UndoBehavior.cs
+++ UndoOperation/UndoBehavior.cs
@@ -11,37 +11,38 @@ using System.Windows.Input;
 
 namespace UndoOperation {
     public class UndoBehavior : Behavior<TableView> {
-        public static readonly DependencyProperty CopyOperationsSupporterProperty =
-           DependencyProperty.Register(nameof(CopyOperationsSupporter), typeof(IDataItemCopyOperationsSupporter), typeof(UndoBehavior), new PropertyMetadata(null));
+        public static readonly DependencyProperty CopyOperationsSupporterProperty;
+        public static readonly DependencyProperty ValidateRowCommandProperty;
+        public static readonly DependencyProperty ValidateRowDeletionCommandProperty;
+
+        static UndoBehavior() {
+            CopyOperationsSupporterProperty = DependencyProperty.Register(nameof(CopyOperationsSupporter), typeof(IDataItemCopyOperationsSupporter), typeof(UndoBehavior), new PropertyMetadata(null));
+            ValidateRowCommandProperty = DependencyProperty.Register(nameof(ValidateRowCommand), typeof(ICommand<RowValidationArgs>), typeof(UndoBehavior), new PropertyMetadata(null));
+            ValidateRowDeletionCommandProperty = DependencyProperty.Register(nameof(ValidateRowDeletionCommand), typeof(ICommand<ValidateRowDeletionArgs>), typeof(UndoBehavior), new PropertyMetadata(null));
+        }
+
+        IList source;
+        Action undoAction;
+        bool isNewItemRowEditing;
+        object editingCache;
 
         public IDataItemCopyOperationsSupporter CopyOperationsSupporter {
             get { return (IDataItemCopyOperationsSupporter)GetValue(CopyOperationsSupporterProperty); }
             set { SetValue(CopyOperationsSupporterProperty, value); }
         }
-
-        public static readonly DependencyProperty ValidateRowCommandProperty =
-          DependencyProperty.Register(nameof(ValidateRowCommand), typeof(ICommand<RowValidationArgs>), typeof(UndoBehavior), new PropertyMetadata(null));
-
         public ICommand<RowValidationArgs> ValidateRowCommand {
             get { return (ICommand<RowValidationArgs>)GetValue(ValidateRowCommandProperty); }
             set { SetValue(ValidateRowCommandProperty, value); }
         }
-
-        public static readonly DependencyProperty ValidateRowDeletionCommandProperty =
-        DependencyProperty.Register(nameof(ValidateRowDeletionCommand), typeof(ICommand<ValidateRowDeletionArgs>), typeof(UndoBehavior), new PropertyMetadata(null));
-
         public ICommand<ValidateRowDeletionArgs> ValidateRowDeletionCommand {
             get { return (ICommand<ValidateRowDeletionArgs>)GetValue(ValidateRowDeletionCommandProperty); }
             set { SetValue(ValidateRowDeletionCommandProperty, value); }
         }
-
         public ICommand UndoCommand { get; private set; }
 
         public UndoBehavior() {
             UndoCommand = new DelegateCommand(Undo, CanUndo);
         }
-        
-        IList source;
 
         protected override void OnAttached() {
             base.OnAttached();
@@ -52,15 +53,6 @@ namespace UndoOperation {
             AssociatedObject.InitNewRow += OnNewRowStarted;
             source = (IList)AssociatedObject.DataControl.ItemsSource;
             AssociatedObject.DataControl.ItemsSourceChanged += ItemsSourceChanged;
-        }
-
-        void ItemsSourceChanged(object sender, ItemsSourceChangedEventArgs e) {
-            source = (IList)AssociatedObject.DataControl.ItemsSource;
-        }
-
-        void OnRefresh(object sender, DataSourceRefreshEventArgs e) {
-            undoAction = null;
-            editingCache = null;
         }
 
         protected override void OnDetaching() {
@@ -74,13 +66,39 @@ namespace UndoOperation {
             base.OnDetaching();
         }
 
+        void ItemsSourceChanged(object sender, ItemsSourceChangedEventArgs e) {
+            source = (IList)AssociatedObject.DataControl.ItemsSource;
+        }
+
+        void OnRefresh(object sender, DataSourceRefreshEventArgs e) {
+            undoAction = null;
+            editingCache = null;
+        }
+
         void OnNewRowStarted(object sender, InitNewRowEventArgs e) {
             isNewItemRowEditing = true;
         }
 
-        bool isNewItemRowEditing;
+        void Undo() {
+            undoAction?.Invoke();
+            undoAction = null;
+        }
+        bool CanUndo() {
+            return undoAction != null && !AssociatedObject.IsEditing && !isNewItemRowEditing && !AssociatedObject.IsDataSourceRefreshing;
+        }
 
-        object editingCache;
+        void UndoEditAction(object item) {
+            ApplyEditingCache(item);
+            AssociatedObject.DataControl.CurrentItem = item;
+        }
+        void UndoAddAction(object item) {
+            AssociatedObject.DataControl.CurrentItem = item;
+            RemoveItem(item);
+        }
+        void UndoDeleteAction(int position, object item) {
+            InsertItem(position, item);
+            AssociatedObject.DataControl.CurrentItem = item;
+        }
 
         void OnEditingStarted(object sender, RowEditStartedEventArgs e) {
             if(e.RowHandle != DataControlBase.NewItemRowHandle) {
@@ -90,21 +108,6 @@ namespace UndoOperation {
 
         void OnRowDeleted(object sender, GridValidateRowDeletionEventArgs e) {
             undoAction = new Action(() => UndoDeleteAction(e.RowHandles.Single(), CopyOperationsSupporter.Clone(e.Rows.Single())));
-        }
-
-        void UndoEditAction(object item) {
-            ApplyEditingCache(item);
-            AssociatedObject.DataControl.CurrentItem = item;
-        }
-
-        void UndoAddAction(object item) {
-            AssociatedObject.DataControl.CurrentItem = item;
-            RemoveItem(item);
-        }
-
-        void UndoDeleteAction(int position, object item) {
-            InsertItem(position, item);
-            AssociatedObject.DataControl.CurrentItem = item;
         }
 
         void OnRowAddedOrEdited(object sender, GridRowValidationEventArgs e) {
@@ -120,17 +123,6 @@ namespace UndoOperation {
             ValidateRowCommand.Execute(new RowValidationArgs(editingCache, source.IndexOf(item), false, new CancellationToken(), false));
         }
 
-        Action undoAction;
-
-        void Undo() {
-            undoAction?.Invoke();
-            undoAction = null;
-        }
-
-        bool CanUndo() {
-            return undoAction != null && !AssociatedObject.IsEditing && !isNewItemRowEditing && !AssociatedObject.IsDataSourceRefreshing;
-        }
-
         void RemoveItem(object item) {
             source.Remove(item);
             ValidateRowDeletionCommand.Execute(new ValidateRowDeletionArgs(new object[] { item }, new int[] { source.IndexOf(item) }));
@@ -140,6 +132,5 @@ namespace UndoOperation {
             source.Insert(position, item);
             ValidateRowCommand.Execute(new RowValidationArgs(item, source.IndexOf(item), true, new CancellationToken(), false));
         }
-
     }
 }
