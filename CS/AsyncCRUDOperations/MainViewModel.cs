a00f2b2fb9bf5e009c0e554fcd1c64d445c0a5cd
CORP\ploskin.alexander <alexander.ploskin@devexpress.com>
2021-11-09 13:25:44 +0300

    Fix source initialization

--- CS/AsyncCRUDOperations/MainViewModel.cs
+++ CS/AsyncCRUDOperations/MainViewModel.cs
@@ -2,31 +2,24 @@
 using DevExpress.Mvvm.DataAnnotations;
 using DevExpress.Mvvm.Xpf;
 using EntityFrameworkIssues.Issues;
-using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AsyncPosting {
+namespace AsyncCRUDOperations {
     public class MainViewModel : ViewModelBase {
         IssuesContext _Context;
-        ObservableCollection<User> _ItemsSource;
+        IList<User> _ItemsSource;
 
-        public ObservableCollection<User> ItemsSource {
+        public IList<User> ItemsSource {
             get {
                 if(_ItemsSource == null && !IsInDesignMode) {
                     _Context = new IssuesContext();
-                    _ItemsSource = new ObservableCollection<User>(_Context.Users);
-                    _ItemsSource.CollectionChanged += _ItemsSource_CollectionChanged;
+                    _ItemsSource = _Context.Users.ToList();
                 }
                 return _ItemsSource;
             }
         }
-
-        private void _ItemsSource_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e) {
-        }
-
         [Command]
         public void ValidateRow(RowValidationArgs args) {
             var item = (User)args.Item;
@@ -37,10 +30,8 @@ namespace AsyncPosting {
                 } else {
                     _Context.SaveChanges();
                 }
-                //throw new Exception("dangerous");
-                return new ValidationErrorInfo("fdsfdsf");
+                return new ValidationErrorInfo("fsdfdsf");
             });
-            //args.Result = "ffs;";
         }
         [Command]
         public void ValidateRowDeletion(ValidateRowDeletionArgs args) {
@@ -51,12 +42,14 @@ namespace AsyncPosting {
         }
         [Command]
         public void DataSourceRefresh(DataSourceRefreshArgs args) {
-            args.RefreshAsync = Task.Run(async () => {
-                await Task.Delay(3000);
-                _ItemsSource = null;
-                _Context = null;
-                RaisePropertyChanged(nameof(ItemsSource));
-            });
+            args.RefreshAsync = RefreshDataSoruceAsync();
+        }
+
+        async Task RefreshDataSoruceAsync() {
+            await Task.Delay(3000);
+            _Context = new IssuesContext();
+            _ItemsSource = _Context.Users.ToList();
+            RaisePropertyChanged(nameof(ItemsSource));
         }
     }
 }
---
+++ CS/AsyncCRUDOperations/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace AsyncCRUDOperations {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
